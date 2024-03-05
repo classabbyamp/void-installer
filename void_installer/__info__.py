ed7942ad2aa3127946434853c831252834639689
classabbyamp <void@placeviolette.net>
2024-03-04 19:08:16 -0500

    basic python package layout and metadata

---
+++ void_installer/__info__.py
@@ -0,0 +1,10 @@
+__project__ = "void_installer"
+__summary__ = "Void Linux installer"
+__webpage__ = "https://github.com/classabbyamp/void-installer"
+
+__version__ = "0.0.1"
+
+__author__ = "classabbyamp"
+__email__ = "void@placeviolette.net"
+
+__license__ = "BSD 2-Clause"
